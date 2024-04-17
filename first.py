ab56f0c3d1f9ff5174653e1238aad257534c298e
javadovrufiq99 <149192043+javadovrufiq99@users.noreply.github.com>
2024-04-17 18:58:50 +0400

    Delete first.py

--- first.py
+++
@@ -1,9 +0,0 @@
-username = input('Enter your username: ')
-age = int(input('Enter your age: '))
-
-child1 = int(input(username+" adlı istifadəçinin 1-ci övladının yaşını qeyd edin: ")) # 6 yaş
-child2 = int(input(username+" adlı istifadəçinin 2-ci övladının yaşını qeyd edin: ")) # 5 yaş
-
-difference = int((age + child1 + child2) / 3)
-
-print(username+' adlı istifadəçinin övladlarının yaş ortalaması ilə '+username+' adlı istifadəçinin yaş ortalaması '+str(difference)+'-dir')
